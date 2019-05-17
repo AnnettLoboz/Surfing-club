b51f6da1695506f01f5d2aa63c0d302211739ce6
Your Name <47412462+AnnettLoboz@users.noreply.github.com>
2019-05-17 22:18:18 +0300

    Fixed shop slider
    (cherry picked from commit 749f0237cf467d985b4eca2f5cc17133e0fd4e71)

---
+++ js/shop-slider.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    var shopSlider = $("#shopSlider");
+    shopSlider.owlCarousel({
+        items: 3,
+        // loop: true,
+        dots: false,
+        smartSpeed: 500,
+    });
+
+});
+
+    // headerSlider.on('initialized.owl.carousel', function(event){
+    //     $('.slide-controls-number__active').text(event.item.index +1);
+    //     $('.slide-controls-number__total').text(event.item.count)
+    // });
+
+    //
+    //
+    //
+    // $('#headerSliderLeft').click(function() {
+    //     console.log('Left');
+    //     headerSlider.trigger('prev.owl.carousel');
+    // })
+    //
+    // $('#headerSliderRight').click(function() {
+    //     console.log('Right');
+    //     headerSlider.trigger('next.owl.carousel');
+    // });
+    // headerSlider.on('changed.owl.carousel', function(event){
+    //     $('.slide-controls-number__active').text(event.item.index +1);
+    //     $('.slide-controls-number__total').text(event.item.count)
+    // });
